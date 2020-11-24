d92d64bdb2b3291f03872aabec1cbf27597aa9e6
Shytikova Yelyzaveta <izuma3kitsune@gmail.com>
2020-11-24 20:36:17 +0200

    fix 3

--- artfreedom/settings.py
+++ artfreedom/settings.py
@@ -24,10 +24,11 @@ sys.path.insert(0, os.path.join(PROJECT_ROOT, "apps"))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w2egfmr9@cjaj#ydbyg+u9dw^7ayczhfeyva2e6fzy^pfeo#9b'
+#SECRET_KEY = 'w2egfmr9@cjaj#ydbyg+u9dw^7ayczhfeyva2e6fzy^pfeo#9b'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'w2egfmr9@cjaj#ydbyg+u9dw^7ayczhfeyva2e6fzy^pfeo#9b')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
@@ -144,3 +145,16 @@ STATICFILES_FINDERS = (
 
 DAJAXICE_MEDIA_PREFIX = 'dajaxice'
 
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
